2fc38be37791badac9071f34def8c0a5163228bc
peter-siebold <peter.siebold@icloud.com>
2018-10-17 11:33:38 +0700

    added typedoc comments

--- src/generic/decorators/staticImplements.ts
+++ src/generic/decorators/staticImplements.ts
@@ -1,4 +1,23 @@
-/* class decorator */
+/**
+ * Decorator that can be used in Combination with an interface to describe static classes
+ * @example
+ * ```
+ * export interface MyClassDescription {
+ *   new (): GenericNodeTransformerBase;
+ *   myStaticMethod(): string;
+ * }
+ *
+ * @staticImplements<MyClassDescription>()
+ * export class MyClass {
+ *   public static myStaticMethod(){
+ *       return `MyClass.myStaticMethod was called`;
+ *   }
+ * }
+ * ```
+ * @export
+ * @template T
+ * @returns
+ */
 export function staticImplements<T>() {
     // tslint:disable-next-line:no-empty
     return (constructor: T) => {};
